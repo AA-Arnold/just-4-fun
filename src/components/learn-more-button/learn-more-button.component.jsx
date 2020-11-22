b174e4690f0585b58f576edca0a177e964144ca1
AA_Arnold <amasoharnold@gmail.com>
2020-11-22 05:44:50 -1100

    added sign up component and improved responsiveness for mobile still a slight problem with iphone 5 and less

---
+++ src/components/learn-more-button/learn-more-button.component.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './learn-more-button.styles.css'
+
+export const LearnMore = ({ children }) => (
+    <a href="#i" className="btn-text">{children}</a>
+)
--- src/pages/hero/hero.jsx
+++ src/pages/hero/hero.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import './hero.css'
 import bana from '../../assets/banner.png'
+import {LearnMore} from '../../components/learn-more-button/learn-more-button.component'
 
 const Hero = () =>(
     <header>
       <div className='banner'>
-        <div className='banner__descr'>
-          <h1 className='banner__heading'>Let's play a game <br /> just for fun</h1>
-          <p className='banner__paragraph'>
-            J4F is an interactive, fun, secure and
-            safe app. Create an account, set your
-            questions, share your profile link and
-            see what your friends think about you!
-          </p>
+        <div className="banne__flex-container">
+          <div className='banner__descr'>
+            <h1 className='banner__heading'>Let's play a game <br /> just for fun</h1>
+            <p className='banner__paragraph'>
+              J4F is an interactive, fun, secure and
+              safe app. Create an account, set your
+              questions, share your profile link and
+              see what your friends think about you!
+            </p>
+            <LearnMore>SIGN UP NOW &rarr;</LearnMore>
+          </div>
+        
+          <img alt='' src={bana} className='banner__img'/>
         </div>
-      
-        <img alt='' src={bana} className='banner__img'/>
+        
       </div>
-     
+      
     </header>
 )
 
