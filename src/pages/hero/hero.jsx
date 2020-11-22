001473e803640bc76909969e64acfe60100a80eb
AA_Arnold <amasoharnold@gmail.com>
2020-11-22 03:40:58 -1100

    completed responsiveness for the landing page

--- src/pages/hero/hero.jsx
+++ src/pages/hero/hero.jsx
@@ -6,8 +6,8 @@ const Hero = () =>(
     <header>
       <div className='banner'>
         <div className='banner__descr'>
-          <h1>Let's play a game <br /> just for fun</h1>
-          <p>
+          <h1 className='banner__heading'>Let's play a game <br /> just for fun</h1>
+          <p className='banner__paragraph'>
             J4F is an interactive, fun, secure and
             safe app. Create an account, set your
             questions, share your profile link and
