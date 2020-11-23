34151a80590d4da5f347694dee12e34293341d4d
AA_Arnold <amasoharnold@gmail.com>
2020-11-23 08:29:16 -1100

    mobile responsiveness complete for features page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './icons-css/icon-font.css'
 import Nav from './components/nav.component/Nav'
 import Hero from './pages/hero/hero'
+import { Features } from './pages/features/feature'
 
 
 const App = () => (
   <div className="App">
     <Nav />
     <Hero />
+    <Features />
   </div>
 )
 
--- src/pages/features/feature.jsx
+++ src/pages/features/feature.jsx
@@ -1,32 +1,41 @@
 import React from 'react'
-import {FeatureBox} from '../../components/feature-box/feature-box.component'
-import './features.scss'
+import {FeatureBox} from '../../components/features/features-box.component'
+import './features.css'
 
 export const Features = () => (
     <section className="section-features">
         <div className="row">
             <div className="col-1-of-4">
                 <FeatureBox heading={'Explore the world'} iconName={'icon-basic-world'}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Exercitationem, necessitatibus! 
+                Your messages are secured with
+                end to end encryption and your 
+                account is secure. J4f is built 
+                with top-notch security
                 </FeatureBox>
             </div>
             <div className="col-1-of-4">
                 <FeatureBox heading={'Meet Nature'} iconName={'icon-basic-compass'}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Exercitationem, necessitatibus! 
+                We are constantly working on
+                J4F as a platform to make 
+                it as user friendly as 
+                possible to enable good user experience
                 </FeatureBox>
             </div>
             <div className="col-1-of-4">
                 <FeatureBox heading={'Find your way'} iconName={'icon-basic-map'}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Exercitationem, necessitatibus! 
+                Set up your account within 
+                seconds and begin having fun 
+                without worrying about any 
+                additional charges what so ever 
                 </FeatureBox>
             </div>
             <div className="col-1-of-4">
                 <FeatureBox heading={'Live a healthier life'} iconName={'icon-basic-heart'}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Exercitationem, necessitatibus! 
+                if there's anything that you
+                need help with feel free to 
+                contact us at any medium we
+                are always available round the
+                clock
                 </FeatureBox>
             </div>      
         </div>
