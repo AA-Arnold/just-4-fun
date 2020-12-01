ed654fc47c91e3480ec083861aef284337ef47e4
AA_Arnold <amasoharnold@gmail.com>
2020-12-01 01:56:43 -1100

    added onclick funtionality that allows the compnent to update

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -3,51 +3,47 @@ import React from 'react'
 import {friendEvalData} from './FriendEvalData'
 import './dashboard.css'
 
-
-
-
 const Dashboard = () =>{
 
     let [questionNumber, setQuestionNumber] = React.useState(0)
 
-    React.useEffect(() => {
-     onNextChange()
-
-     //eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [questionNumber])
-
     const onNextChange = () =>{
-        if(questionNumber < 10){
-            setQuestionNumber(questionNumber++)
+        
+        if(questionNumber < 9){
+
+            setQuestionNumber((questionNumber) => questionNumber + 1)
         }
     }
 
-    // React.useEffect(() => {
-    //     const onNextChange = () =>{
-    //         if(questionNumber < 10){
-    //             setQuestionNumber(questionNumber++)
-    //         }
-    //     }
-    //     onNextChange()
-    //    }, [questionNumber])
+    const onPrevChange = () =>{
 
+        if (questionNumber > 0){
+
+            setQuestionNumber((questionNumber) => questionNumber - 1)
+
+        }
+    }
+
+    const getTargetHtml = e => console.log(e.target )
 
     return(
-        <div className='friendeval'>
+       <div className='friendeval'>
        <h2 className='friendeval__header'>Friend Evaluation</h2> 
-       <div className='friendeval__question-count'>Question 1/10</div> 
+       <div className='friendeval__question-count'>Question {friendEvalData[questionNumber].questionId}/10</div> 
         <div className='align'>
             <img className='friendeval__image'alt='' src={friendEvalData[questionNumber].image}/> 
         </div>
        <div className='friendeval__question'>{friendEvalData[questionNumber].question}</div>
        <div className="friendeval__buttons">
            {
-               friendEvalData[questionNumber].options.map(option => <button>{option}</button>)
+
+               friendEvalData[questionNumber].options.map(option => <button onClick={getTargetHtml}>{option}</button>)
+               
            }
 
        </div>
        <div className="friendeval__nav">
-           <button >Prev</button>
+           <button onClick={onPrevChange} >Prev</button>
            <button onClick={onNextChange}>Next</button>
        </div>
     </div>
